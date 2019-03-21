a6de21ae7112a9600e373b52d6f39d78c891977b
guokai <1124046469@qq.com>
2019-03-21 21:28:54 +0800

    把本地工程添加到git

---
+++ kaixuan/src/cn/dadi/FirstClass.java
@@ -0,0 +1,12 @@
+package cn.dadi;
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("hell git project");
+        System.out.println("hell git project");
+        System.out.println("hell git project");
+        System.out.println("hell git project");
+        System.out.println("hell git project");
+        System.out.println("hell git project");
+
+    }
+}
